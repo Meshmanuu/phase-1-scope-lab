13bbc8f66cf2c492f68cbe40594e2e1129c69a17
Meshmanuu <meshanyangu@gmail.com>
2024-03-24 18:04:06 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+// 1) Fix the Scope
+var customerName = 'bob';
+
+function returnCustomerName() {
+  return customerName;
+}
+
+// 2) Fix the Scope
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3) Fix the Scope
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// 4) Fix the Scope
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// 5) Fix the Scope
+const leastFavoriteCustomer = 'not bob';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else';
+}
+
+
